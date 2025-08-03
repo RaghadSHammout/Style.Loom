1156396c16394127b27a71269bb6e0822755e612
zainbsalman-zqs <zainbsalman24@gmail.com>
2025-08-03 16:53:12 +0300

    navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Products from './pages/Products.tsx'
+import Contact from './pages/Contact.tsx'
+const routers = createBrowserRouter([
+  {
+     path: "/",
+    element: <App/>,
+      children: [
+      {
+        path: "",
+        element: <Home/>,
+      },
+      {
+        path: "/products",
+        element: <Products/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      },
+    ],
+  }
+],
+ {
+    basename: "/Style.Loom-x3", 
+  }
+)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </StrictMode>,
 )

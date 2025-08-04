d3a24e17b3e662be1add2c6b1cec30c5c3266ff1
wafaa-aa <fyfy3thman@gmail.com>
2025-08-04 12:49:05 +0300

    first Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+import FilterTabs from "./components/FilterTabs"
+import { ProductTabsData } from "./components/data/FilterTabsData"
+
 function App() {
+  const [activeTab , setActiveTab] = useState("All")
   return (
     <div className="bg-primarybg min-h-screen text-white">
+      <FilterTabs 
+      tabs={ProductTabsData} 
+       activeTab={activeTab}
+       onChange={(tab)=>  setActiveTab(tab)}
+       />
     </div>
   )
 }

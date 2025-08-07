b4172e7f6fef872f3f616c5ab6dc1a27ca3bda51
MayaAlhalak <msy768464@gmail.com>
2025-08-07 14:52:11 +0300

    reusable

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,25 @@
+import { useSelector } from "react-redux"
+import Cards, { ShowImageType } from "../components/Cards"
+
 function Contact() {
+  
+ const Card3 = useSelector(((state: any) => state.cards.cardthree))
   return (
-    <div>Contact</div>
+    <div>
+           <div className="flex flex-wrap justify-center">
+       {Array.isArray(Card3) &&
+        Card3.map((item , index) => (
+          <Cards
+          key={index}
+          showimage = {ShowImageType.three}
+         img={item.img}
+          title={item.title}
+          description={item.description}
+     />
+        ))  
+      }
+     </div>
+    </div>
   )
 }
 

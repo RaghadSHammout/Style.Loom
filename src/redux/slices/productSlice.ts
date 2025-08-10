cbe133841924b92ec4d074a028a2ddb0b5c7208e
wafaa-aa <fyfy3thman@gmail.com>
2025-08-10 15:44:04 +0300

    update

---
+++ src/redux/slices/productSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { ProductsCardsData } from "../../data/ProductsCardsData";
+import type { FilterType, ProductState } from "../../types";
+
+const initialState: ProductState = {
+    allProducts: ProductsCardsData,
+    activeType: 'All',
+    filteredProducts: ProductsCardsData,
+};
+const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {
+        setActiveType(state, action: PayloadAction<FilterType>) {
+            state.activeType = action.payload;
+
+            if (action.payload === "All") {
+                state.filteredProducts = state.allProducts;
+            } else {
+                const filtered = state.allProducts
+                    .filter(product => product.type === action.payload)
+                    .sort(
+                        (a, b) =>
+                            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+                    )
+                    .slice(0, 6);
+
+                state.filteredProducts = filtered;
+            }
+        },
+    },
+});
+
+export const { setActiveType } = productSlice.actions;
+export default productSlice.reducer;
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -4,3 +4,34 @@ export interface FilterTabsProps {
     type?: "Hero";
     onChange: (tab: string) => void;
 }
+export interface ProductCardProps {
+    ProductImage: string;
+    category: string;
+    ProductName: string;
+    Fitvalue: string;
+    Pricevalue: string
+
+}
+export interface FitPriceRowProps {
+    label: string;
+    value: string
+}
+export type ProductType = "Woman" | "Man" | "Kids";
+export type FilterType = ProductType | "All";
+
+export interface Product {
+    id: number;
+    type: ProductType;
+    ProductImage: string;
+    category: string;
+    ProductName: string;
+    Fitvalue: string;
+    Pricevalue: string;
+    createdAt: string;
+}
+
+export interface ProductState {
+    allProducts: Product[];
+    activeType: FilterType;
+    filteredProducts: Product[];
+}

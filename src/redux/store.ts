20f44c49795a90933ce9d025a26ce25b6eab5e9d
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-08-11 13:34:19 +0300

    i finished the section of testmonials

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import dashboardReducer from "./slices/dashboardSlice"; // <-- تأكدي من المسار
 
-import cardsReducer from './slice/index';
-import navReducer from './slices/index';
+import cardsReducer from "./slice/index";
+import navReducer from "./slices/index";
+import testmonialsReducer from "./testmonials/index";
 
 export const store = configureStore({
   reducer: {
-        dashboard: dashboardReducer, 
-
+    dashboard: dashboardReducer,
     cards: cardsReducer,
     nav: navReducer,
+    testmonials: testmonialsReducer,
   },
 });
 

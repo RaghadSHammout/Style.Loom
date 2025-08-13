81cccd53b561ec2a0edd0987f64228327f419c93
RaghadSHammout <raghadshammout3010@gmail.com>
2025-08-13 13:13:06 +0300

    fix reusable section problems

--- src/components/ReusableSection.tsx
+++ src/components/ReusableSection.tsx
@@ -25,26 +25,22 @@ function ReusableSection<T extends string = string>({
         max-2xl:pb-[60px] max-2xl:pl-[60px] max-lg:gap-[30px]  max-lg:pt-[30px] max-lg:pr-[20px] max-lg:pb-[30px] max-lg:pl-[20px] `}
       >
         <div className={`font-robotom flex flex-col gap-[30px] max-2xl:gap-6 max-lg:gap-5`}>
-          <h2 className={font-medium text-5xl leading-[100%] tracking-normal uppercase max-2xl:text-[38px] max-lg:text-[28px] }>
+          <h2 className="font-medium text-5xl leading-[100%] tracking-normal uppercase max-2xl:text-[38px] max-lg:text-[28px]">
             {heading}
           </h2>
-          <p className={text-gray-40 font-normal text-lg leading-[150%] tracking-normal max-2xl:text-base max-lg:text-sm }>
+          <p className="text-gray-40 font-normal text-lg leading-[150%] tracking-normal max-2xl:text-base max-lg:text-sm">
             {text}
           </p>
         </div>
 
         {image && (
-          <div className={absolute right-0 top-0 overflow-hidden max-lg:hidden }>
-            <img src={image} alt={alt} className={overflow-hidden ${imgwidth}} />
+          <div className="absolute right-0 top-0 overflow-hidden max-lg:hidden">
+            <img src={image} alt={alt} className={`overflow-hidden ${imgwidth}`} />
           </div>
         )}
 
         {showTabs && activeTab && tabs && onChange && (
-          <FilterTabs
-            tabs={tabs}
-            activeTab={activeTab}
-            onChange={onChange}
-          />
+          <FilterTabs tabs={tabs} activeTab={activeTab} onChange={onChange} />
         )}
       </div>
 

4a0c8d545115ee607e7bcc106086a9d5ebd773e2
RaghadSHammout <raghadshammout3010@gmail.com>
2025-08-14 20:17:58 +0300

    Fixed some problems

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import productReducer from "./slices/productSlice";        
-import dashboardReducer from "./slices/dashboardSlice";
 
 import cardsReducer from "./slice";                        
 import navReducer from "./slices";                         
@@ -10,7 +9,6 @@ import testmonialsReducer from "./testmonials";
 export const store = configureStore({
   reducer: {
     product: productReducer,       
-    dashboard: dashboardReducer,
     cards: cardsReducer,
     nav: navReducer,
     testmonials: testmonialsReducer, 

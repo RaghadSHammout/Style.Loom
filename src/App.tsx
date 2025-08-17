ab4e57d6e2cd1735ea8e8faedc509e8b20e07143
zainbsalman-zqs <zainbsalman24@gmail.com>
2025-08-17 14:18:14 +0300

    sectionContactData

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-primarybg text-white dark:bg-primarybg dark:text-white">
+    <div className="min-h-screen  text-white dark:bg-primarybg dark:text-white">
       <NavBar />
       <HeroSection />
       <Outlet />

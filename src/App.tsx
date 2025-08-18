af125be3f51b6baba46809a53c02b3af1a970486
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-08-18 18:22:30 +0300

    make testimonials array dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom"
-import NavBar from "./components/NavBar"
-import CallToAction from "./components/CallToAction"
-import { CtaData } from './data/CtaData';
-import HeroSection from "./components/HeroSection"
+// import NavBar from "./components/NavBar"
+// import CallToAction from "./components/CallToAction"
+// import { CtaData } from './data/CtaData';
+// import HeroSection from "./components/HeroSection"
 
 function App() {
   return (
     <div className="bg-primarybg min-h-screen text-white">
-      <NavBar/>  
-      <HeroSection />
+      {/* <NavBar/>  
+      <HeroSection /> */}
       <Outlet /> 
 {/* for foter CTA */}
 
-    <CallToAction heading={CtaData.heading} text={CtaData.text}  image={CtaData.image} alt={CtaData.alt}/>
+    {/* <CallToAction heading={CtaData.heading} text={CtaData.text}  image={CtaData.image} alt={CtaData.alt}/> */}
     </div>
   )
 }

1e50ceb4adc7046d4fb381d7956cd534f2bdde35
RaghadSHammout <raghadshammout3010@gmail.com>
2025-08-22 22:01:03 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -31,7 +31,7 @@ const routers = createBrowserRouter(
       ],
     },
   ],
-  { basename: '/Style.Loom-x3' }
+  { basename: '/Style.Loom' }
 );
 
 createRoot(document.getElementById('root')!).render(

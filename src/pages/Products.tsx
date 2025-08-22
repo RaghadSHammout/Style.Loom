aac4facaa5d55c4462d1b5854d2034fef5c9f7e1
zainbsalman-zqs <zainbsalman24@gmail.com>
2025-08-22 20:03:43 +0300

    Products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,41 +1,64 @@
 import { useDispatch, useSelector } from "react-redux";
-import ReusableSection from "../components/ReusableSection"
-import { ProductTabsData } from "../data/FilterTabsData";
-import { ProductsSection } from "../data/ReusableSectionData";
-import type { RootState } from "../redux/store";
-import type { FilterType } from "../types";
-import { setFilteredSections } from "../redux/slices/productSlice";
 import { useEffect } from "react";
+
+import ReusableSection from "../components/ReusableSection";
 import ProductsContainer from "../components/ProductsContainer";
+import TestmonialsCards from "../components/TestmonialsCards";
+import QuestionsCards from "../components/QuestionsCards";
 
+import { ProductTabsData, tabsFaq } from "../data/FilterTabsData";
+import { SectionDataProducts, sectionData3, baseFaqData } from "../data/ReusableSectionData";
 
+import { setFilteredSections } from "../redux/slices/productSlice";
+import { setActiveTab } from "../redux/questions"; 
+
+import type { RootState } from "../redux/store";
+import type { FilterType, FilterFaqType } from "../types";
+import PageTransitionWrapper from "../components/PageTransitionWrapper";
 
 function Products() {
   const dispatch = useDispatch();
+  const faqDispatch = useDispatch();
 
-  const activeTab = useSelector((state: RootState) => state.product.activeTypeForSections);
-  const sections = useSelector((state: RootState) => state.product.filteredSections);
+  
+  const productActiveTab = useSelector(
+    (state: RootState) => state.product.activeTypeForSections
+  );
+  const sections = useSelector(
+    (state: RootState) => state.product.filteredSections
+  );
 
   const handleTabChange = (tab: FilterType) => {
     dispatch(setFilteredSections(tab));
   };
 
   useEffect(() => {
-    dispatch(setFilteredSections(activeTab));
-  }, [dispatch]);
+    dispatch(setFilteredSections(productActiveTab));
+  }, [dispatch, productActiveTab]);
 
-  const sectionProductsData = {
-    ...ProductsSection,
+  const productsSection = {
+    ...SectionDataProducts,
     tabs: ProductTabsData,
-    activeTab,
+    activeTab: productActiveTab,
     onChange: handleTabChange,
     showTabs: true,
   };
 
-  return (
+  
+  const faqActiveTab = useSelector((state: RootState) => state.faq.activeTab);
+  const filteredFaqs = useSelector((state: RootState) => state.faq.filteredFaqs);
 
-    <div className="p-[16px] md:p[80px] 2xl:p-[162px] ">
-      <ReusableSection {...sectionProductsData}>
+  const faqSection = {
+    ...baseFaqData,
+    tabs: tabsFaq,
+    activeTab: faqActiveTab,
+    onChange: (tab: FilterFaqType) => faqDispatch(setActiveTab(tab)),
+  };
+
+  return (
+     <PageTransitionWrapper>
+      <div className="pt-[223px] max-lg:pt-[185px] max-md:pt-[166px] pb-[100px] max-lg:pb-[80px] max-md:pb-[50px] 2xl:px-[162px] lg:px-[80px] px-[16px]">
+      <ReusableSection {...productsSection}>
         {sections.map((section) => (
           <ProductsContainer
             key={section.category}
@@ -43,10 +66,19 @@ function Products() {
             products={section.products}
           />
         ))}
+      </ReusableSection>
 
+      <ReusableSection {...sectionData3}>
+        <TestmonialsCards />
+      </ReusableSection>
+
+      <ReusableSection {...faqSection}>
+        <QuestionsCards filteredFaqs={filteredFaqs} />
       </ReusableSection>
     </div>
-  )
+     </PageTransitionWrapper>
+    
+  );
 }
 
-export default Products
+export default Products;
